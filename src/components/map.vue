1f2385a3646d138b3a717afe0d9ab38b66432f33
Akulllvlllad <Akulllvlllad@gmail.com>
2024-12-02 15:58:22 +0700

    save

--- src/components/map.vue
+++ src/components/map.vue
@@ -3,14 +3,15 @@
         <h2 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10"> Где мы находимя? </h2>
         <div style="width: 100%">
             <iframe 
-                width="100%" 
-                height="600" 
-                frameborder="0" 
-                scrolling="no" 
-                marginheight="0" 
-                marginwidth="0" 
-                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%E2%80%8B%D0%90%D1%82%D0%BC%D0%BE%D1%81%D1%84%D0%B5%D1%80%D0%B0%20%D0%B4%D0%BE%D0%BC%D0%B0,%20%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%92%D0%B0%D0%B2%D0%B8%D0%BB%D0%BE%D0%B2%D0%B0%20%D1%83%D0%BB%D0%B8%D1%86%D0%B0,%201%20%D1%81%D1%8239,%20%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D1%8F%D1%80%D1%81%D0%BA+(English%20time)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                </iframe></div>
+            width="100%" 
+            height="600" 
+            frameborder="0" 
+            scrolling="no" 
+            marginheight="0" 
+            marginwidth="0" 
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=ru&amp;q=%E2%80%8B%D0%90%D1%82%D0%BC%D0%BE%D1%81%D1%84%D0%B5%D1%80%D0%B0%20%D0%B4%D0%BE%D0%BC%D0%B0,%20%D0%90%D0%BA%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%D0%BA%D0%B0%20%D0%92%D0%B0%D0%B2%D0%B8%D0%BB%D0%BE%D0%B2%D0%B0%20%D1%83%D0%BB%D0%B8%D1%86%D0%B0,%201%20%D1%81%D1%8239,%20%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D1%8F%D1%80%D1%81%D0%BA+(English%20time)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+        </iframe>
+        </div>
 
     </div>
 </template>
