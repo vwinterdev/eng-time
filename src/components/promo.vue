71b209cbbd8944bdbeb151cb18c31cb742841bd0
Akulllvlllad <Akulllvlllad@gmail.com>
2024-12-02 15:52:48 +0700

    save

--- src/components/promo.vue
+++ src/components/promo.vue
@@ -2,7 +2,7 @@
     <div class="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <p class="text-sm leading-6 text-white">
         <a href="#">
-          <strong class="font-semibold">GeneriCon 2023</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Join us in Denver from June 7 – 9 to see what’s coming next&nbsp;<span aria-hidden="true">&rarr;</span>
+          Актуальную информацию уточняйте по номеру <a href="tel:+79999999999"></a> +7&nbsp;(999)&nbsp;999-99-99&nbsp;
         </a>
       </p>
       <div class="flex flex-1 justify-end">
