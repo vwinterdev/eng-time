ba34785f3b2019070a45ca75b060b476028895ac
Akulllvlllad <Akulllvlllad@gmail.com>
2024-12-02 18:41:02 +0700

    save

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { ViteSSG } from 'vite-ssg/single-page'
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Widget from './widget.vue'
 
-ViteSSG(App)
-createApp(Widget).mount('#widget')
+export const createApp = ViteSSG(App)
