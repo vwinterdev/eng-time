3867d13e7880c7638471a2b836caa5f5627043a1
Akulllvlllad <Akulllvlllad@gmail.com>
2024-12-02 17:20:10 +0700

    save

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -36,14 +36,14 @@
           <div class="overflow-hidden">
             <div class="mx-auto max-w-7xl px-6 pb-32 pt-20 sm:pt-20 lg:px-8 lg:pt-10">
               <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
-                <!-- <div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+                <div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                         <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl typing">Успех в английском начинается с English Time!</h1>
                   <p class="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">Наша школа предлагает качественное обучение английскому и французскому языкам для всех уровней подготовки – от начального до продвинутого. Мы используем современные методики преподавания, которые помогают нашим студентам быстро и эффективно освоить язык. 
                     .</p>
                   <div class="mt-10 flex items-center gap-x-6">
                     <a href="#form" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Отправить заявку</a>
                   </div>
-                </div> -->
+                </div>
                 <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-10 lg:pl-0">
                   <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <div class="relative element">
