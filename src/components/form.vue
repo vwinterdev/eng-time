e46587f5eabf4826633b1e4dec08892bad38e2ee
Akulllvlllad <Akulllvlllad@gmail.com>
2024-12-04 13:23:06 +0700

    save

--- src/components/form.vue
+++ src/components/form.vue
@@ -60,7 +60,7 @@
             </dl>
           </div>
         </div>
-        <form action="#" onclick="sendEmail" class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+        <form onsubmit="send(event)" class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48" >
           <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
